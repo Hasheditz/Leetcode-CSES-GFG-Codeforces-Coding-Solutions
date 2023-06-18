a1248188d74b95a313718be40046dd9fe14aa355
hasheditz <harendrasingh2406@gmail.com>
2023-06-18 21:57:59 +0530

    18-06-23

---
+++ June/18June.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int distributeTicket(int n, int k) {
+        // code here
+         int grp = n/k;
+        int left = (grp/2)*k;
+        int right = n - (grp/2)*k + 1;
+
+        if(right - left > (k + 1))
+            return left + k + 1;
+
+        return right - (right - left != 1);
+    }
+    
+    
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int N;
+        scanf("%d",&N);
+        
+        
+        int K;
+        scanf("%d",&K);
+        
+        Solution obj;
+        int res = obj.distributeTicket(N, K);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
