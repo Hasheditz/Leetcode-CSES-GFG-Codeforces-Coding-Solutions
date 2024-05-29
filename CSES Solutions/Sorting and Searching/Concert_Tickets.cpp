a882fa46d2afee354c78f0262d9eb56461e5fb78
Harendra singh <94151837+Hasheditz@users.noreply.github.com>
2024-05-29 15:36:25 +0530

    Update Concert_Tickets.cpp

--- CSES Solutions/Sorting and Searching/Concert_Tickets.cpp
+++ CSES Solutions/Sorting and Searching/Concert_Tickets.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+ 
 #define pb push_back
 #define ppb pop_back
 #define mp make_pair
@@ -11,20 +11,44 @@ using namespace std;
 #define all(x) (x).begin(), (x).end()
 #define nl endl
 #define ll long long int
-
-
-
+ 
 void solve()
 {
-    
+    int n,m;
+    cin>>n>>m;
+ 
+    multiset<int> ticket;
+ 
+    for(int i = 0 ; i < n ; i++)
+    {
+        int x;
+        cin>>x;
+        ticket.insert(x);
+    }
+ 
+    for(int i = 0 ; i < m ; i++)
+    {
+        int curr;
+        cin>>curr;
+ 
+        auto it = ticket.upper_bound(curr);
+ 
+        if(it == ticket.begin())
+        {
+            cout<<-1<<endl;
+        }
+ 
+        else {cout<<*(--it)<<endl; ticket.erase(it);}
+    }
+ 
 }
-
+ 
 int main()
 {
-    int t;
-    cin >> t;
-
-    while (t--)
+    // int t;
+    // cin >> t;
+ 
+    // while (t--)
     {
         solve();
     }
